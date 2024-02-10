49f109a264df44efd1b3ed09fa7aa050fb26e27c
Walter Jenkins <walter.k.jenkins@gmail.com>
2024-02-09 19:10:46 -0600

    add andres files

---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/bin/zsh
+
+# install/update homebrew
+which -s brew
+if [[ $? != 0 ]] ; then
+  echo "Homebrew not found. Installing..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+  echo "Homebrew already installed. Updating..."
+  brew update
+  brew upgrade
+fi
+
+# # install from Brewfile
+echo "Installing from Brewfile"
+brew bundle --file ./Brewfile
+
+# install zsh-zap
+echo "Installing Zap"
+zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh)
+
+# set up symlinks 
+echo "Creating symlinks"
+stow */
+
+# set iTerm2 prefs custom folder
+echo "Setting up iTerm's prefs custom folder"
+defaults write com.googlecode.iterm2 PrefsCustomFolder -string "~/.dotfiles/iterm2"
+defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
+
+# setup lazyvim
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+# required
+mv ~/.config/nvim{,.bak}
+
+# optional but recommended
+mv ~/.local/share/nvim{,.bak}
+mv ~/.local/state/nvim{,.bak}
+mv ~/.cache/nvim{,.bak}
+
+cp .config/nvim/ ~/.config/nvim/
+
+echo "All done 🎉"
