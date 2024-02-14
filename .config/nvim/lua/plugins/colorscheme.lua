3a9acf4f115f2399a85e4a914661070bd0157193
Walter Jenkins <walter.k.jenkins@gmail.com>
2024-02-14 09:21:20 -0600

    updated theme and fixed linting

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,7 @@
 return {
 	-- add gruvbox
-	{ "kjssad/quantum.vim" },
+	{ "AvidDabbler/quantum.vim" },
+	-- { "kjssad/quantum.vim" },
 
 	-- Configure LazyVim to load gruvbox
 	{
@@ -8,14 +9,5 @@ return {
 		opts = {
 			colorscheme = "quantum",
 		},
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		config = function()
-			vim.cmd([[highlight LineNr guifg=#aeb7ae]])
-			vim.cmd([[highlight CursorLine guibg=#044c02]])
-			vim.cmd([[highlight CursorLineNr guifg=#df83cb]])
-			vim.cmd([[highlight Visual guibg=#586857]])
-		end,
 	},
 }
